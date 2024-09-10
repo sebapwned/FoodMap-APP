38fe341d7041025cda3c625cbe4894dbc62bb0ed
sebapwned <seb.viveros@duocuc.cl>
2024-09-10 02:06:12 -0300

    Falta solucionar problemas de responsabilidad en la vista de Login, el diseño esta completo en un 85%

--- src/app/managers/SessionManager.ts
+++ src/app/managers/SessionManager.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 
 export class SessionManager {
 
-    private readonly temporaryUserName: string = 'user';
-    private readonly temporaryPass: string = 'pass';
+    private readonly temporaryUserName: string = 'foodadmin';
+    private readonly temporaryPass: string = 'foodpassword';
 
     performLogin(user: string, password: string): boolean {
         if(user == this.temporaryUserName && password == this.temporaryPass) {
