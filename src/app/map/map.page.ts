d6ce5ba2d634e30973fe05dc08a9ec4fc446459d
sebapwned <seb.viveros@duocuc.cl>
2024-12-05 05:47:40 -0300

    Proyecto funcional, rubrica completa, listo para instalarle android studio

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { ActivatedRoute, Router } from '@angular/router';
-/// <reference types="@types/google.maps" />
+import { NavController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue, DatabaseReference } from 'firebase/database';
+import { environment } from 'src/environments/environment';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-map',
@@ -9,24 +13,37 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  map!: google.maps.Map;
+  restaurants: any[] = [];
 
   constructor(
     private navCtrl: NavController,
-    private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private toastController: ToastController,
+    private firestore: AngularFirestore
   ) {}
 
   ngOnInit() {
-    this.loadMap();
-  }
-
-  loadMap() {
-    const mapOptions = {
-      center: { lat: -33.0336352, lng: -71.5344662 },
-      zoom: 12,
-    };
-    this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, mapOptions);
+    try {
+      // Realizamos una consulta a Firestore para obtener todos los restaurantes
+      this.firestore.collection('restaurants').valueChanges({ idField: 'id' }).subscribe(
+        (data) => {
+          if (data.length > 0) {
+            this.restaurants = data;
+            console.log('Datos obtenidos desde Firestore:', data);
+          } else {
+            this.restaurants = [];
+            console.warn('No se encontraron restaurantes.');
+          }
+        },
+        (error) => {
+          console.error('Error al obtener los restaurantes:', error);
+          this.presentToast('Error al cargar los restaurantes. Inténtalo de nuevo más tarde.');
+        }
+      );
+    } catch (error) {
+      console.error('Error durante la inicialización de la página:', error);
+      this.presentToast('Error de conexión. Verifica tu configuración.');
+    }
   }
 
   goBack() {
@@ -39,5 +56,15 @@ export class MapPage implements OnInit {
 
   onHomeButtonPressed() {
     this.router.navigate(['/inicio']);
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+      position: 'bottom',
+      color: 'danger'
+    });
+    toast.present();
   }
 }
