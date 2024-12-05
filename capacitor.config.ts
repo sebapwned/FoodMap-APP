c67614af4eb84cf8fb25e60fff64c0d27254b450
sebapwned <seb.viveros@duocuc.cl>
2024-12-05 08:12:24 -0300

    Proyecto probado en emulador de Android Studio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'FoodMapAPPMovil',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false
 };
 
 export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+
+    apiKey: "AIzaSyBAg3mz91MR47WMIETEGaJBJuxExYuFzJg",
+  
+    authDomain: "foodmapappmovil.firebaseapp.com",
+  
+    projectId: "foodmapappmovil",
+  
+    storageBucket: "foodmapappmovil.firebasestorage.app",
+  
+    messagingSenderId: "931785137593",
+  
+    appId: "1:931785137593:web:ec98fb4115bbf1d2c26a68"
+  }
 };
